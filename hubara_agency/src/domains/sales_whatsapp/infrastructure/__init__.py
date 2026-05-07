128122164921ae51850f00a0feda6bd6638fd098
edgm <einsteindark@gmail.com>
2026-05-07 16:43:09 -0500

    Refactor Agentes Arquitectura propia

--- hubara_agency/src/domains/sales_whatsapp/infrastructure/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Infraestructura del dominio Sales (DEHA).
-
-Adapters concretos que cumplen los `application/ports/*`. Solo aqui se hace
-filesystem I/O, llamadas a Temporal client desde el lado HTTP, etc.
-"""
